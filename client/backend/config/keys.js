4c74482a74e2b99c23b17df789c19d2c63c683e7
yousseflaaroui3010 <youssef.laaroui@etu.uae.ac.ma>
2025-04-12 22:59:46 +0100

    deleted

--- client/backend/config/keys.js
+++
@@ -1,5 +0,0 @@
-// client/backend/config/keys.js
-module.exports = {
-  mongoURI: process.env.MONGO_URI || "mongodb+srv://yousseflaaroui1:mSL5RHKrvf67VmoI@cluster0.4xwma.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
-  jwtSecret: process.env.JWT_SECRET || "votre_secret_jwt"
-};
