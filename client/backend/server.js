6247ff085feea1fb922c66e0e871e257559362b6
yousseflaaroui3010 <youssef.laaroui@etu.uae.ac.ma>
2025-04-12 16:43:02 +0100

    client/frontend/build

--- client/backend/server.js
+++ client/backend/server.js
@@ -33,10 +33,10 @@ app.use('/api/products', require('./routes/product'));
 // Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static('../frontend/build'));
+  app.use(express.static('client/frontend/build'));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../frontend', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client/frontend', 'build', 'index.html'));
   });
 }
 
